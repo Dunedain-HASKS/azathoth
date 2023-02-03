4c63dd1054ea747f6a7fc3e789fd3a23fa6f07cf
sohamviradiya <sohamviradiya2003@gmail.com>
2023-02-04 02:07:45 +0530

    add stocks router

--- main.controller.js
+++ main.controller.js
@@ -6,17 +6,12 @@ const passport = require('passport');
 const router = express.Router();
 const userRouter = require('./user/user.controller');
 const companiesRouter = require('./companies/companies.controller');
-    
-router.use('/user',userRouter);
+const stocksRouter = require('./stocks/stocks.controller');
+router.use('/user', userRouter);
 router.use('/companies', companiesRouter);
-
-
-const stock = require('./stocks/stocks.controller');
-
-router.use('/stock', stock);
+router.use('/stocks', stocksRouter);
 
 module.exports = router;
-     
 
 
 //Phase 2
