4794a5e1afe43a9afb05bb35cc3a9cd302b004f3
harshal2804 <harshal5820@gmail.com>
2023-02-03 23:16:05 +0530

    create user GET and POST

--- user/user.controller.js
+++ user/user.controller.js
@@ -5,3 +5,67 @@
 // /schemes
 // /transactions
 // 
+import User from './user.schema'
+const router = require('express').Router()
+
+
+router.get("/user", async (req, res) => {
+    try {
+        const user = await User.find({ email: req.user.email })
+        if (user) {
+            res.json({
+                status: 200,
+                message: '',
+                data: user
+            })
+        } else {
+            res.json({
+                status: 401,
+                message: 'User dont exists',
+                data: {}
+            })
+        }
+    } catch(err) {
+        res.json({
+            status: 401,
+            message: err,
+            data: {}
+        })
+    }
+})
+
+router.post("/user", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.user.email })
+        if (user) {
+            return res.json({
+                status: 401,
+                message: 'User already exists!',
+                data: {}
+            })
+        }
+
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.user.password, salt);
+        const data = { ...req.user, password: hashedPassword };
+
+        User.save(data, (err, collection) => {
+            if (err) {
+                throw err;
+            }
+        });
+        res.json({
+            status: 200,
+            message: '',
+            data: data
+        })
+    } catch(err) {
+        res.json({
+            status: 401,
+            message: err,
+            data: {}
+        })
+    }
+})
+
+module.exports = router
