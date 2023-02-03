e49831ffb49d5c6db10aa22149dbd2146f25f6b8
sohamviradiya <sohamviradiya2003@gmail.com>
2023-02-03 22:28:46 +0530

    add main router

--- main.controller.js
+++ main.controller.js
@@ -1,6 +1,14 @@
 //Phase 1
 //stock,company,user routes
+const express = require('express');
+
 //authentication
+
+const router = express.Router();
+
+
+export default router;
+
 
 //Phase 2
 //schemes
