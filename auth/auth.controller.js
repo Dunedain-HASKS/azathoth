9d68e81218dde11149a96c58317fb4bb0f5013c6
sohamviradiya <sohamviradiya2003@gmail.com>
2023-02-03 23:35:52 +0530

    add passport

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -1,3 +1,8 @@
 // /auth/login
 // /auth/create
 // /auth/
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+// /auth/login using email and password
